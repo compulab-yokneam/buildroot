0f858e9a01efcf86da891b60b6029011c7c47dce
Valentin Raevsky <valentin@compulab.co.il>
2023-09-24 11:28:07 +0000

    buildroot: board: Update post-image.sh
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- buildroot/board/compulab-aarch64-efi/post-image.sh
+++ buildroot/board/compulab-aarch64-efi/post-image.sh
@@ -2,4 +2,6 @@
 
 BOARD_DIR="$(dirname $0)"
 
+UUID=$(dumpe2fs ${BINARIES_DIR}/rootfs.ext2  2>/dev/null | awk '(/Filesystem UUID:/)&&($0=$NF)')
 cp -f ${BOARD_DIR}/grub.cfg ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
+sed -i "s/UUID_TMP/$UUID/g" ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
